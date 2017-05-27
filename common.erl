a6ae70d1a3ed73c25fb3dc51dc655b286b4564cb
Lukasz Sowinski <niman.gosen.en@gmail.com>
2017-05-27 10:47:20 +0900

    stored first 1000000 in a file for faster algorithms setup

--- common.erl
+++ common.erl
@@ -1,5 +1,5 @@
 -module(common).
--export([product/1, pow10/1, pown/2, gcd/2, snd/1, fst/1]).
+-export([product/1, pow10/1, pown/2, gcd/2, snd/1, fst/1, savePrimes/0, loadPrimes/0]).
 
 product(L) -> lists:foldl(fun (X, A) -> X * A end, 1, L).
 
@@ -15,3 +15,16 @@ gcd(A, B) -> gcd(B, A rem B).
 
 snd({_, Y}) -> Y.
 fst({X, _}) -> X.
+
+savePrimes() -> 
+    {ok, File} = file:open("primes.txt", [write]),
+	Primes = lists:reverse(euler10:findPrimes(1000000)),
+    lists:map(fun(N) -> file:write(File, integer_to_list(N) ++ "\n") end, Primes),
+    file:position(File, {eof, -1}),
+    file:truncate(File).
+
+loadPrimes() -> 
+    {ok, Data} = file:read_file("primes.txt"),
+    lists:map(fun binary_to_integer/1, binary:split(Data, [<<"\n">>], [global])).
+
+
